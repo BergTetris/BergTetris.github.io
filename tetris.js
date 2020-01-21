e15958eacaea7737358053332c2174aa262cc265
bergmaner <bergkacper@gmail.com>
2020-01-21 18:28:46 +0100

    add collide and stick function and arena
    Now, it's possible to drop element to arena.
    Arrangment of the elements on arena put on Array.
    We can put elements on other elements and bottom border.
    Merge of elements with left or right border is impossible.
    We can move elements only on arena.
    Exit of elements outside from arena is impossible.

--- tetris.js
+++ tetris.js
@@ -12,10 +12,55 @@ const piece =
 [0,1,0],
 ];
 
+const arena = createPiece(12,20);
+
+function collide(arena,player)
+{
+
+    const [p,o] = [player.piece,player.position];
+    
+    for(let y=0; y < p.length; y++)
+    {
+        for(let x=0; x < p[y].length; x++)
+        {
+            if(p[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x +o.x])!==0)
+            {
+                return true;
+            }
+            
+        }
+    }
+    return false;
+}
+
+function createPiece(w,h)
+{
+    const piece = [];
+    while(h--)
+    {
+        piece.push(new Array(w).fill(0));
+    }
+    return piece;
+}
+
+function stick(arena,player)
+{
+    player.piece.forEach((row,y) =>{
+
+        row.forEach((value,x) =>{
+        
+            if(value !== 0)
+            {
+                arena[y + player.position.y][x + player.position.x] = value;
+            }
+        });
+    });
+}
 function draw()
 {
     ctx.fillStyle='#000';
     ctx.fillRect(0,0,canvas.width,canvas.height);
+    drawPiece(arena,{x:0,y:0});
     drawPiece(player.piece,player.position);
 }
 
@@ -35,13 +80,11 @@ function drawPiece(piece,offset)
 function update(time=0)
 {
     const deltaTime = time - lastTime;
-    console.log(deltaTime);
     lastTime = time;
     dropCounter += deltaTime;
     if (dropCounter > dropInterval)
     {
-        player.position.y++;
-        dropCounter = 0;
+       playerDrop();
     }
     draw();
     requestAnimationFrame(update);
@@ -50,6 +93,13 @@ function update(time=0)
 function playerDrop()
 {
     player.position.y++;
+    if(collide(arena,player))
+    {
+        player.position.y--;
+        stick(arena,player);
+        player.position.y =0;
+    }
+    
     dropCounter = 0;
 }
 
@@ -58,13 +108,23 @@ const player = {
     piece:piece,
 }
 
+playerMove(value)
+{
+    player.position.x += value;
+    if(collide(arena,player))
+    {
+        player.position.y -= value;
+    }
+
+}
+
 document.addEventListener('keydown',e => {
 if( e.keyCode === 37 || e.keyCode === 65){
-player.position.x--;
+playerMove(-1);
 }
 else if( e.keyCode === 39 || e.keyCode === 68 ){
-    player.position.x++;
-    }
+    playerMove(1);
+}
     else if( e.keyCode === 40 || e.keyCode === 83 ){
         playerDrop();
         }
